67a894001f68fdc88279c7f66990afd60d0edc5e
Kenny <mccormick84@gmail.com>
2022-02-15 23:18:35 +0900

    프로젝트 생성

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var switchValue = false;
+
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+          body: Center(
+            child: Switch(
+                value: switchValue,
+                onChanged: (value) {
+                  switchValue = value;
+                }),
+          ),
+        ));
+  }
+}
